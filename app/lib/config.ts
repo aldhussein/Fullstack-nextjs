ea532bcd136e3e1b3e55b85d4d62e79f1a18dd61
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-29 13:36:07 +0300

    Update config.ts

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '' :
+export const API_URL = process.env.NODE_ENV === 'production' ? 'fullstack-nextjs-hugffgco6-aldhusseins-projects.vercel.app' :
 'http://localhost:3000/';
