f1daefb56ad497aaac44dbaab10167edcd20e955
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-29 12:37:07 +0300

    Update next.config.ts
    another ignoring

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint : {
-    ignoreDuringBuilds: true
+  // Your other config options can go here
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
