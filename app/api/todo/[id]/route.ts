93d26a7fbe621882a06740ba4f181c32d710f024
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-29 14:05:05 +0300

    Update route.ts

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -2,28 +2,28 @@ import connectToTheDatabase from "@/app/lib/db";
 import TodoModel from "@/app/models/todoModel";
 import { NextRequest, NextResponse } from "next/server";
 
-type RouteContext = {
-  params: {
-    id: string;
-  };
-};
-
-export async function PUT(req: NextRequest, context: RouteContext) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Record<string, string> }
+) {
   await connectToTheDatabase();
 
-  const { id } = context.params;
   const isDone = await req.json();
 
-  const updatedTodo = await TodoModel.findByIdAndUpdate(id, { isDone });
+  const updatedTodo = await TodoModel.findByIdAndUpdate(params.id, {
+    isDone,
+  });
 
   return NextResponse.json(updatedTodo, { status: 201 });
 }
 
-export async function DELETE(req: NextRequest, context: RouteContext) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Record<string, string> }
+) {
   await connectToTheDatabase();
 
-  const { id } = context.params;
-  const deletedTodo = await TodoModel.findByIdAndDelete(id);
+  const deletedTodo = await TodoModel.findByIdAndDelete(params.id);
 
   return NextResponse.json(deletedTodo, { status: 201 });
 }
